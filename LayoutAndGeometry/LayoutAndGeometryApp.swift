15eb033c4539f65d4de021399575347a4dcbd308
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-09 13:13:03 -0600

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Juan Carlos Robledo Morales on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LayoutAndGeometryTests/LayoutAndGeometryTests.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryTests.swift
+//  LayoutAndGeometryTests
+//
+//  Created by Juan Carlos Robledo Morales on 09/12/24.
+//
+
+import Testing
+@testable import LayoutAndGeometry
+
+struct LayoutAndGeometryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
