d75e2c86f70c3dbe4349a79b0bbd8854070b42d5
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-10 13:54:01 -0600

    Understanding frames and coordinates inside GeometryReader

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,24 +8,59 @@
 import SwiftUI
 
 
-struct ContentView: View {
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            GeometryReader { proxy in
+//                Text("Hello, World!")
+//                    .frame(width: proxy.size.width * 0.9, height: 40)
+//                    .background(.red)
+//            }
+//            .background(.green)
+//
+//            Text("More text")
+//                .background(.blue)
+//        }
+//    }
+//}
+
+struct OuterView: View {
     var body: some View {
-        HStack{
-            Text("IMPORTANT")
-                .frame(width: 200)
-                .background(.blue)
-            
-            GeometryReader { proxy in
-                Image(.cybuster)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: proxy.size.width * 0.8)
-                    .frame(width: proxy.size.width, height: proxy.size.height)
-            }
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
         }
     }
 }
 
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
+    }
+}
 
 #Preview {
     ContentView()
