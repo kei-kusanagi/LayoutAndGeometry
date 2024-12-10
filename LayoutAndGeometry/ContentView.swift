1b6020391da324d8a5711d8533ada1c11a18a39f
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-09 18:08:16 -0600

    Alignment and alignment guides

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,16 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    //    var body: some View {
+    //        VStack(alignment: .leading) {
+    //            Text("Hello, world!")
+    //                .alignmentGuide(.leading) { d in d[.trailing] }
+    //            Text("This is a longer line of text")
+    //        }
+    //        .background(.red)
+    //        .frame(width: 400, height: 400)
+    //        .background(.blue)
+    //    }
     var body: some View {
-//        Text("Hello, World!")
-//            .padding(20)
-//            .background(.red)
-        
-//        Text("Hello, World!")
-//            .padding()
-//            .background(.red)
-
-        Color.red
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in Double(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
