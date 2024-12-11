b72def4cb5ba5d58c10223d4177c95ea8cf52b15
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-10 18:50:45 -0600

    ScrollView effects using visualEffect() and scrollTargetBehavior()

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,52 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     
     
     var body: some View {
         
-        ScrollView {
-                    ForEach(0..<50) { index in
-                        GeometryReader { proxy in
-                            Text("Row #\(index)")
-                                .font(.title)
-                                .frame(maxWidth: .infinity)
-                                .background(colors[index % 7])
-                        }
-                        .frame(height: 40)
-                    }
-                }
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(1..<20) { num in
-                    GeometryReader { proxy in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(
+                                    .degrees(-proxy.frame(in: .global).minX) / 8,
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
+                        }
                 }
             }
+            .scrollTargetLayout()
         }
-        
-GeometryReader { fullView in
-           ScrollView {
-               ForEach(0..<50) { index in
-                   GeometryReader { proxy in
-                       Text("Row #\(index)")
-                           .font(.title)
-                           .frame(maxWidth: .infinity)
-                           .background(colors[index % 7])
-                           .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-                   }
-                   .frame(height: 40)
-               }
-           }
-       }
+        .scrollTargetBehavior(.viewAligned)
+
     }
 }
 
